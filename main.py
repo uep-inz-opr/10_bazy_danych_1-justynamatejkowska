bacf9410cb127795d805a76476bb7012eadc3eac
Justyna <j.matejkowska@gmail.com>
2022-06-04 14:34:55 +0200

    2 wersja

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import csv, sqlite3
+
 if __name__ == "__main__":
-    
-    sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    cur = sqlite_con.cursor()
 
-    cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
-                      to_subscriber data_type INTEGER, 
-                      datetime data_type timestamp, 
-                      duration data_type INTEGER , 
-                      celltower data_type INTEGER);''') # use your column names here
+  !curl https://raw.githubusercontent.com/khashishin/repozytorium_z_plikiem_polaczenia/main/polaczenia_duze.csv > polaczenia_duze.csv
 
-    #Otwieramy plik.csv i przerzucamy go w najprostszy sposób do naszej nowo utworzonej bazy sqlite
-    with open('polaczenia_duze.csv','r') as fin: 
-        # csv.DictReader uses first line in file for column headings by default
-        reader = csv.reader(fin, delimiter = ";") 
-        next(reader, None) 
-        rows = [x for x in reader]
-        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows) #wartości oznaczane 
-        sqlite_con.commit() 
+  sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+  cur = sqlite_con.cursor()
 
-    sqlite_con
-    cursor=sqlite_con.cursor()
-    cursor.execute("Select sum(duration) from polaczenia")
-    result = cursor.fetchall()
+  cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+                    to_subscriber data_type INTEGER, 
+                    datetime data_type timestamp, 
+                    duration data_type INTEGER , 
+                    celltower data_type INTEGER);''') 
+
+  with open('polaczenia_duze.csv','r') as fin: 
+      reader = csv.reader(fin, delimiter = ";") 
+      next(reader, None)
+      rows = [x for x in reader]
+      cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows) #wartości oznaczane 
+      sqlite_con.commit() 
+
+  sqlite_con
+  cursor=sqlite_con.cursor()
+  cursor.execute("Select sum(duration) from polaczenia")
+  result = cursor.fetchone()
+
+  res = int(''.join(map(str, result)))
+
+print(str(res))
